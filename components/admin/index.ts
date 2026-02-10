0ecdd606526f4bf5601d8a94bc04017df69023fc
do0ik <oulwind00@naver.com>
2026-02-11 03:29:36 +0900

    chore: 미사용 파일 정리

--- components/admin/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as UsersManagement } from './UsersManagement';
-export { default as UserStatsCards } from './UserStatsCards';
-export { default as UserTable } from './UserTable';
-export { default as UserActions } from './UserActions';
-export { default as UserDetailModal } from './UserDetailModal';
--- components/calendar/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ProjectCalendar } from './ProjectCalendar'
